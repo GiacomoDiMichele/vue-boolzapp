165da9d515c92712f39f870cad365d635aa95bd1
Giacomo <giacomo.1936@gmail.com>
2020-11-12 16:57:22 +0100

    creato v-for per lista-contatti

---
+++ main.js
@@ -0,0 +1,33 @@
+var app = new Vue (
+    {
+        el: '#root',
+        data: {
+            contacts: [
+                {
+                    name: 'Alberto',
+                    avatar: 'alberto.png',
+                },
+                {
+                    name: 'Chiara',
+                    avatar: 'alberto.png',
+                },
+                {
+                    name: 'Chiara',
+                    avatar: 'alberto.png',
+                },
+                {
+                    name: 'Chiara',
+                    avatar: 'alberto.png',
+                },
+                {
+                    name: 'Chiara',
+                    avatar: 'alberto.png',
+                },
+                {
+                    name: 'Chiara',
+                    avatar: 'alberto.png'
+                }
+            ]
+        }
+    }
+)
