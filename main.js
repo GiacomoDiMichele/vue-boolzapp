4cce652716fe8fd006911dee6655415653130132
Giacomo <giacomo.1936@gmail.com>
2020-11-13 15:29:42 +0100

    aggiunto v for  per visualizzazione dinamica dei messaggi

--- main.js
+++ main.js
@@ -5,27 +5,135 @@ var app = new Vue (
             contacts: [
                 {
                     name: 'Alberto',
-                    avatar: 'img/alberto.png'
+                    avatar: 'img/alberto.png',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '',
+                            message: 'Ricordati di dargli a mangiare',
+                            status: 'sent'
+                        },
+                        {
+                            date: '',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ]
                 },
                 {
                     name: 'Chiara',
-                    avatar: 'img/alberto.png'
+                    avatar: 'img/chiara.png',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '',
+                            message: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '',
+                            message: 'bene grazie, stasera usciamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '',
+                            message: 'Mi piacerebbe ma devo andare a fare la spesa!',
+                            status: 'sent'
+                        }
+                    ]
                 },
                 {
                     name: 'Francesca',
-                    avatar: 'img/alberto.png'
+                    avatar: 'img/francesca.png',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '',
+                            message: 'Come sta marco?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '',
+                            message: 'meglio non ha più la febbre',
+                            status: 'sent'
+                        },
+                        {
+                            date: '',
+                            message: 'Ottimo domani passo a salutarlo',
+                            status: 'received'
+                        }
+                    ]
                 },
                 {
                     name: 'Francesco',
-                    avatar: 'img/alberto.png'
+                    avatar: 'img/francesco.png',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '',
+                            message: 'Ricordati di dargli a mangiare',
+                            status: 'sent'
+                        },
+                        {
+                            date: '',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ]
                 },
                 {
                     name: 'Mario',
-                    avatar: 'img/alberto.png'
+                    avatar: 'img/mario.png',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '',
+                            message: 'Ricordati di dargli a mangiare',
+                            status: 'sent'
+                        },
+                        {
+                            date: '',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ]
                 },
                 {
                     name: 'Chiara',
-                    avatar: 'img/alberto.png'
+                    avatar: 'img/chiara.png',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '',
+                            message: 'Ricordati di dargli a mangiare',
+                            status: 'sent'
+                        },
+                        {
+                            date: '',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ]
                 }
             ]
         }
